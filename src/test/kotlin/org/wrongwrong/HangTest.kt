b2dbeb8a68be084690aa82b0d08b18eed080f5fb
wrongwrong <boranti1995@gmail.com>
2024-06-03 15:04:09 +0900

    Add tests to select
    However, hangs are not reproduced

--- src/test/kotlin/org/wrongwrong/HangTest.kt
+++ src/test/kotlin/org/wrongwrong/HangTest.kt
@@ -1,26 +1,55 @@
 package org.wrongwrong
 
 import io.r2dbc.spi.ConnectionFactories
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.awaitSingle
 import kotlinx.coroutines.runBlocking
+import org.jooq.Condition
 import org.jooq.DSLContext
+import org.jooq.Record
+import org.jooq.SQLDialect
+import org.jooq.TableField
 import org.jooq.generated.tables.records.AccountsRecord
 import org.jooq.generated.tables.references.ACCOUNTS
 import org.jooq.impl.DSL
+import org.jooq.impl.DSL.field
+import org.jooq.impl.DSL.min
+import org.jooq.impl.DSL.name
 import org.junit.jupiter.api.Test
+import org.wrongwrong.HangTest.Companion.INFINITE_DATETIME
 import java.time.OffsetDateTime
 import java.util.*
+import kotlin.system.measureTimeMillis
+
+fun <T : TableField<R, OffsetDateTime?>, R : Record> biTemporalCurrentStatusCondition(
+    validFrom: T,
+    validTo: T,
+    transactTo: T,
+): Condition {
+    return field(validFrom.name).le(DSL.currentOffsetDateTime())
+        .and(field(validTo.name).gt(DSL.currentOffsetDateTime()))
+        .and(field(transactTo.name).eq(INFINITE_DATETIME))
+}
+
+fun <T : TableField<R, OffsetDateTime?>, R : Record> biTemporalValidStatusesCondition(transactTo: T): Condition {
+    return transactTo.eq(INFINITE_DATETIME)
+}
 
 class HangTest {
     companion object {
-        const val SIZE = 10000
+        const val SIZE = 1500000
         val INFINITE_DATETIME: OffsetDateTime = OffsetDateTime.parse("2200-12-31T23:59:59Z")
+
+        const val TEMP_ACCOUNTS = "temp_accounts"
+        const val ACCOUNT_ID = "account_id"
+        const val CREATED_AT = "created_at"
     }
 
     val factory =
         ConnectionFactories.get("r2dbc:postgresql://jooq-16669-root:jooq-16669-root@localhost:5432/jooq-16669-db")
-    val factoryCtxt = DSL.using(factory)
-    val connectionCtxt = runBlocking { DSL.using(factory.create().awaitSingle()) }
+    val factoryCtxt = DSL.using(factory, SQLDialect.POSTGRES)
+    val connectionCtxt = runBlocking { DSL.using(factory.create().awaitSingle(), SQLDialect.POSTGRES) }
 
     fun resetTestData(ctxt: DSLContext) = runBlocking {
         ctxt.deleteFrom(ACCOUNTS).awaitSingle()
@@ -56,10 +85,47 @@ class HangTest {
                     )
                     .awaitSingle()
             }
-    }.apply { println("beforeAll completed") }
+    }.apply { println("$SIZE records created") }
+
+    fun select(ctxt: DSLContext) = ctxt.with(TEMP_ACCOUNTS)
+        .`as`(
+            DSL.select(ACCOUNTS.ID.`as`(ACCOUNT_ID), min(ACCOUNTS.VALID_FROM).`as`(CREATED_AT))
+                .from(ACCOUNTS)
+                .where(biTemporalValidStatusesCondition(ACCOUNTS.TRANSACT_TO))
+                .groupBy(ACCOUNTS.ID)
+        )
+        .select(ACCOUNTS.ID, ACCOUNTS.TEL, ACCOUNTS.EMAIL)
+        .from(ACCOUNTS)
+        .innerJoin(TEMP_ACCOUNTS)
+        .on(ACCOUNTS.ID.eq(field(name(TEMP_ACCOUNTS, ACCOUNT_ID), UUID::class.java)))
+        .asFlow()
+
+    fun repeatSelectTest(ctxt: DSLContext) {
+        println("start repeatSearch\n")
+
+        (1..10).forEach {
+            runBlocking {
+                val time = measureTimeMillis {
+                    val result = select(ctxt).toList()
+                    println(result.size)
+                }
+
+                println("${String.format("%03d", it)}: $time")
+                println()
+            }
+        }
+    }
 
     @Test
     fun test() {
-        resetTestData(connectionCtxt)
+        try {
+            resetTestData(connectionCtxt)
+
+            repeatSelectTest(factoryCtxt)
+            repeatSelectTest(connectionCtxt)
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            throw e
+        }
     }
 }
